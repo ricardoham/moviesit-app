75695338dc76d8b11d07a51f6873b7d6f9f9d770
Ricardo Manoel <rdomanoel@gmail.com>
2021-07-03 15:40:30 -0300

    add eslint configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Home from 'pages/Home';
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      <span>Works </span>
+      <Home />
     </div>
   );
 }
