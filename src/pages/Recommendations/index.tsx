599de273215c6e82a95ccc0d59e87683e5619442
Ricardo Manoel <rdomanoel@gmail.com>
2021-09-10 20:55:16 -0300

    fix reco layout

--- src/pages/Recommendations/index.tsx
+++ src/pages/Recommendations/index.tsx
@@ -7,27 +7,22 @@ const Recommendations = (): JSX.Element => {
   const history = useHistory();
 
   return (
-    <Box bg="white" m={2}>
-      <Box>
-        <Card
-          image="/images/myreco.jpg"
-          header="Minhas recomendações"
-          text="Acesse suas recomendações ou crie uma nova"
-          btnText="Ver mais..."
-          onAction={() => history.push('/recommendations/myrecommendations')}
-        />
-      </Box>
-      <Box>
-        <Card
-          invert
-          image="/images/recommendations.jpg"
-          header="Recomendações da comunidade"
-          text="Veja as últimas recomendações ou pesquisa uma por usuário ou titulo"
-          btnText="Ver mais..."
-          onAction={() => history.push('/recommendations/community')}
-        />
-      </Box>
-
+    <Box bg="white" h="110vh" display="flex" flexFlow="column">
+      <Card
+        image="/images/myreco.jpg"
+        header="Minhas recomendações"
+        text="Acesse suas recomendações ou crie uma nova"
+        btnText="Ver mais..."
+        onAction={() => history.push('/recommendations/myrecommendations')}
+      />
+      <Card
+        invert
+        image="/images/recommendations.jpg"
+        header="Recomendações da comunidade"
+        text="Veja as últimas recomendações de filmes da comunidade"
+        btnText="Ver mais..."
+        onAction={() => history.push('/recommendations/community')}
+      />
     </Box>
   );
 };
